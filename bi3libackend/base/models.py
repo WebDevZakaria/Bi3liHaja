0e5ff63fd011891720dc45c1c8180a1483e8bc7f
zakaria bouregbi <zakariabouregbidev@gmail.com>
2024-01-01 13:57:02 +0100

    add Order and shipping Models

--- bi3libackend/base/models.py
+++ bi3libackend/base/models.py
@@ -59,5 +59,69 @@ class Product(models.Model):
     def __str__(self):
 
         return self.name
+    
+class Order(models.Model):
+
+    user = models.ForeignKey(Account, on_delete=models.SET_NULL, null=True)
+
+    textPrice = models.DecimalField(
+        max_digits=7, decimal_places=2, null=True, blank=True)
+    
+    shippingPrice = models.DecimalField(
+        max_digits=7, decimal_places=2, null=True, blank=True)
+    totalPrice = models.DecimalField(
+        max_digits=7, decimal_places=2, null=True, blank=True)
+    isPaid = models.BooleanField(default=False)
+
+    paidAt = models.DateTimeField(auto_now_add=False, null=True, blank=True)
+
+    isDelivred = models.BooleanField(default=False)
+
+    deliveredAt = models.DateTimeField(
+        auto_now_add=False, null=True, blank=True)
+
+    createdAt = models.DateTimeField(auto_now_add=True)
+
+    _id = models.AutoField(primary_key=True, editable=False)
+
+    def __str__(self):
+
+        return str(self.createdAt)
+
+
+class OrderItem(models.Model):
+
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    price = models.DecimalField(
+        max_digits=7, decimal_places=2, null=True, blank=True)
+    image = models.CharField(max_length=200, null=True, blank=True)
+    _id = models.AutoField(primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.name)
+    
+class ShippingAddress(models.Model):
+
+    order = models.OneToOneField(
+        Order, on_delete=models.CASCADE, null=True, blank=True)
+
+    address = models.CharField(max_length=200, null=True, blank=True)
+
+    city = models.CharField(max_length=200, null=True, blank=True)
+
+    phone_number = models.CharField(max_length=200, null=True, blank=True)
+
+    wilaya = models.CharField(max_length=200, null=True, blank=True)
+
+    shippingPrice = models.DecimalField(
+        max_digits=7, decimal_places=2, null=True, blank=True)
+
+    _id = models.AutoField(primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.address)
+
 
 
